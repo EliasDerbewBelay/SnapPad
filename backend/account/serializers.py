e5c24cbbe5a96527ad050d0fd06be9cc2f785d92
eliasderbewbelay <eliasderbew1@gmail.com>
2026-02-22 14:05:16 +0300

    serializing the end points of the account app

---
+++ backend/account/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'display_name', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email = validated_data['email'],
+            password = validated_data['password'],
+            display_name = validated_data.get('display_name', '')
+        )
+
+        return user
